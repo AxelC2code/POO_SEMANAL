2940313ef9e76b201c245c4dedab8b820f818d23
Axl'HH <caxel0624@gmail.com>
2026-08-26 12:01:38 -0500

    chore(sem 02): update ejercicio 08
    T: trabajador

--- src/Sem02/Ejec_Trabajador.java
+++ src/Sem02/Ejec_Trabajador.java
@@ -1,20 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Sem02;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class Ejec_Trabajador {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Prueba estática
+        String p_nom = "Carlos";
+        double p_phora = 15.5; 
+        double p_horas = 40;
+        
+        Trabajador obj_1 = new Trabajador();
+        obj_1.set_nombre(p_nom);
+        obj_1.set_precioHora(p_phora); 
+        obj_1.set_horasTrabajadas(p_horas);
+
+        System.out.println("Trabajador: " + obj_1.get_nombre());
+        System.out.println("Salario Bruto: " + obj_1.calcularSalarioBruto());
+        System.out.println("Impuesto: " + obj_1.calcularImpuesto());
+        System.out.println("Salario Neto: " + obj_1.calcularSalarioNeto());
+
+        // Prueba dinámica
+        Scanner obj_teclado = new Scanner(System.in);
+        System.out.print("Ingrese nombre del trabajador (sin espacios): "); 
+        p_nom = obj_teclado.next();
+        System.out.print("Ingrese precio por hora: "); 
+        p_phora = obj_teclado.nextDouble();
+        System.out.print("Ingrese horas trabajadas: "); 
+        p_horas = obj_teclado.nextDouble();
+        
+        Trabajador obj_2 = new Trabajador(p_nom, p_phora, p_horas);
+
+        System.out.println("Salario Bruto: " + obj_2.calcularSalarioBruto());
+        System.out.println("Impuesto: " + obj_2.calcularImpuesto());
+        System.out.println("Salario Neto: " + obj_2.calcularSalarioNeto());
     }
-    
 }
--- src/Sem02/Trabajador.java
+++ src/Sem02/Trabajador.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Sem02;
 
-/**
- *
- * @author Usuario
- */
 public class Trabajador {
-    
+    // Atributos
+    private String nombre;
+    private double precioHora;
+    private double horasTrabajadas;
+
+    // Constructores
+    public Trabajador(String p_nombre, double p_precioHora, double p_horasTrabajadas) {
+        this.nombre = p_nombre;
+        this.precioHora = p_precioHora;
+        this.horasTrabajadas = p_horasTrabajadas;
+    }
+
+    public Trabajador() {
+        this.nombre = "";
+        this.precioHora = 0;
+        this.horasTrabajadas = 0;
+    }
+
+    // Getters y Setters
+    public void set_nombre(String p_nombre) {
+        this.nombre = p_nombre;
+    }
+
+    public String get_nombre() {
+        return this.nombre;
+    }
+
+    public void set_precioHora(double p_precioHora) {
+        this.precioHora = p_precioHora;
+    }
+
+    public double get_precioHora() {
+        return this.precioHora;
+    }
+
+    public void set_horasTrabajadas(double p_horasTrabajadas) {
+        this.horasTrabajadas = p_horasTrabajadas;
+    }
+
+    public double get_horasTrabajadas() {
+        return this.horasTrabajadas;
+    }
+
+    // Métodos
+    public double calcularSalarioBruto() {
+        return this.precioHora * this.horasTrabajadas;
+    }
+
+    public double calcularImpuesto() {
+        return this.calcularSalarioBruto() * 0.10; // 10 por ciento
+    }
+
+    public double calcularSalarioNeto() {
+        return this.calcularSalarioBruto() - this.calcularImpuesto();
+    }
 }
