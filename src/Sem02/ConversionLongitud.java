e6be387e7075ed8905ba132b234cac3c6ed70c47
Axl'HH <caxel0624@gmail.com>
2026-08-26 11:56:26 -0500

    chore(sem 02): update ejercicio 06
    conversion longitudinal

--- src/Sem02/ConversionLongitud.java
+++ src/Sem02/ConversionLongitud.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Sem02;
 
-/**
- *
- * @author Usuario
- */
 public class ConversionLongitud {
+    // Atributos
+    private double metros;
+
+    // Constructores
+    public ConversionLongitud(double p_metros) {
+        this.metros = p_metros;
+    }
+
+    public ConversionLongitud() {
+        this.metros = 0;
+    }
+
+    // Getters y Setters
+    public void set_metros(double p_metros) {
+        this.metros = p_metros;
+    }
+
+    public double get_metros() {
+        return this.metros;
+    }
+
+    // Métodos
+    public double convertirPies() {
+        // 1 metro equivale a 3.28084 pies
+        return this.metros * 3.28084;
+    }
     
+    public double convertirPulgadas() {
+        // 1 metro equivale a 39.3701 pulgadas
+        return this.metros * 39.3701;
+    }
 }
--- src/Sem02/Ejec_ConversionLongitud.java
+++ src/Sem02/Ejec_ConversionLongitud.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Sem02;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class Ejec_ConversionLongitud {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Prueba estática
+        double p_metros = 10.5; 
+        
+        ConversionLongitud obj_1 = new ConversionLongitud();
+        obj_1.set_metros(p_metros); 
+
+        double v_pies = obj_1.convertirPies();
+        double v_pulgadas = obj_1.convertirPulgadas();
+
+        System.out.println("La longitud en pies es: " + v_pies);
+        System.out.println("La longitud en pulgadas es: " + v_pulgadas);
+
+        // Prueba dinámica
+        Scanner obj_teclado = new Scanner(System.in);
+        System.out.print("Ingrese longitud en metros: "); 
+        p_metros = obj_teclado.nextDouble();
+        
+        ConversionLongitud obj_2 = new ConversionLongitud(p_metros);
+        v_pies = obj_2.convertirPies(); 
+        v_pulgadas = obj_2.convertirPulgadas();
+
+        System.out.println("Pies: " + v_pies);
+        System.out.println("Pulgadas: " + v_pulgadas);
     }
-    
 }
